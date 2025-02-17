c0b1b372e433cdb369761b9bf87eec3abac99f31
侑介 <spark9487111@gmail.com>
2025-02-17 18:55:02 +0800

    feat: add logout

--- lib/widget/DrawerWidget.dart
+++ lib/widget/DrawerWidget.dart
@@ -28,8 +28,9 @@ class _DrawerWidgetState extends State<DrawerWidget> {
         padding: EdgeInsets.zero,
         children: <Widget>[
           DrawerHeader(
-            child: Text(
-                SharedPreferencesHelper.getString("USER.NAME") ?? "Not Login"),
+            child: Text(SharedPreferencesHelper.getString("USER.TOKEN") != null
+                ? SharedPreferencesHelper.getString("USER.NAME") ?? "Not Login"
+                : "Not Login"),
           ),
           ListTile(
             title: const Text('Reset Shared Preferences'),
@@ -59,7 +60,8 @@ class _DrawerWidgetState extends State<DrawerWidget> {
                             ),
                             const SizedBox(height: 8),
                             Text(
-                                "Are you sure you want to Reset shared preferences?"),
+                              "Are you sure you want to Reset shared preferences?",
+                            ),
                             const SizedBox(height: 16),
                             Row(
                               mainAxisAlignment: MainAxisAlignment.end,
@@ -95,6 +97,73 @@ class _DrawerWidgetState extends State<DrawerWidget> {
               );
             },
           ),
+          if (SharedPreferencesHelper.getString("USER.TOKEN") != null)
+            ListTile(
+              title: const Text('Logout'),
+              trailing: const Icon(Icons.logout_sharp),
+              onTap: () async {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Dialog(
+                      backgroundColor: Colors.transparent,
+                      child: Material(
+                        child: Container(
+                          padding: const EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Logout Account",
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                "Are you sure you want to Logout?",
+                              ),
+                              const SizedBox(height: 16),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(),
+                                    child: const Text(
+                                      "Cancel",
+                                    ),
+                                  ),
+                                  const SizedBox(width: 8),
+                                  TextButton(
+                                    onPressed: () async {
+                                      await SharedPreferencesHelper.remove(
+                                          "USER.TOKEN");
+                                      await SharedPreferencesHelper.remove(
+                                          "USER.RECOMMENDER.UUID");
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text(
+                                      "Logout",
+                                      style: TextStyle(color: Colors.red),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
         ],
       ),
     );
