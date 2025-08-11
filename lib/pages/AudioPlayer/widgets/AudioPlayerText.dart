5c3a4af15604d61bbd21a45e497b448d2e769f64
侑介 <spark9487111@gmail.com>
2025-08-11 15:53:52 +0800

    style: add padding to audio player text

--- lib/pages/AudioPlayer/widgets/AudioPlayerText.dart
+++ lib/pages/AudioPlayer/widgets/AudioPlayerText.dart
@@ -5,20 +5,23 @@ import 'package:flutter/material.dart';
 import 'package:kikoeru/core/config/provider/AudioProvider.dart';
 
 Widget AudioPlayerText(AudioProvider audioProvider) {
-  return Column(
-    children: [
-      Text(
-        audioProvider.currentAudioTitle ?? "",
-        style: TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
+  return Padding(
+    padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+    child: Column(
+      children: [
+        Text(
+          audioProvider.currentAudioTitle ?? "",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-      ),
-      SizedBox(height: 30),
-      Text(
-        audioProvider.currentAudioWorkTitle ?? "",
-        style: TextStyle(fontSize: 14),
-      ),
-    ],
+        SizedBox(height: 30),
+        Text(
+          audioProvider.currentAudioWorkTitle ?? "",
+          style: TextStyle(fontSize: 14),
+        ),
+      ],
+    ),
   );
 }
