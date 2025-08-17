cfd9754fcff84c1d66c45555d8621456c277ef81
侑介 <spark9487111@gmail.com>
2025-08-17 23:20:24 +0800

    feat: add voteStatus display

--- lib/class/CardWidgets/Tag.dart
+++ lib/class/CardWidgets/Tag.dart
@@ -10,7 +10,7 @@ import 'package:kikoeru/api/WorkRequest/httpRequests.dart';
 // pages
 import 'package:kikoeru/pages/HomePage/pages/SearchPage.dart';
 
-Widget getTagWidget(BuildContext context, String tag) {
+Widget getTagWidget(BuildContext context, (String, int) tag) {
   return GestureDetector(
     child: Container(
       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
@@ -18,10 +18,13 @@ Widget getTagWidget(BuildContext context, String tag) {
         color: Colors.grey.shade800,
         borderRadius: BorderRadius.circular(12),
       ),
-      child: Text(
-        tag,
-        style: const TextStyle(color: Colors.white, fontSize: 15),
-      ),
+      child: Text(tag.$1,
+          style: tag.$2 == 0
+              ? TextStyle(
+                  color: Color.fromARGB(140, 255, 255, 255),
+                  decoration: TextDecoration.underline,
+                )
+              : TextStyle(color: Colors.white, fontSize: 15)),
     ),
     onTap: () {
       Navigator.push(
@@ -29,7 +32,7 @@ Widget getTagWidget(BuildContext context, String tag) {
         MaterialPageRoute(
           builder: (context) => SearchWorksPage(
             type: SearchType.Tag,
-            query: tag,
+            query: tag.$1,
           ),
         ),
       );
@@ -43,9 +46,10 @@ Widget getTag(BuildContext context, Work work) {
     child: Wrap(
       spacing: 6,
       runSpacing: 4,
-      children: work.Tags.map((tag) => tag?["i18n"]["zh-cn"]["name"] ?? "")
-          .where((tag) => tag.isNotEmpty)
-          .map((tag) => getTagWidget(context, tag))
+      children: work.Tags.map(
+              (tag) => (tag?["i18n"]["zh-cn"]["name"], tag?["voteStatus"]))
+          .where((tag) => tag.$1.isNotEmpty)
+          .map((tag) => getTagWidget(context, tag as (String, int)))
           .toList(),
     ),
   );
